f5a91cb9899cae645d2f5e77a6a8460e985b18c9
Muthu <subudhimuthu@gmail.com>
2026-03-23 09:50:04 +0530

    Update no9.py

--- Extra Questions 3/no9.py
+++ Extra Questions 3/no9.py
@@ -1,17 +1,6 @@
 import numpy as np
-arr = np.array([[1,2,3,4],[5,6,7,8]])
-arr2 = np.array([[9,10], [11,12], [13,14], [15,16]])
-x1, y1 = arr.shape
-x2, y2 = arr2.shape
-arr3 = np.zeros((x1, y2))
-print(x1,y1,x2,y2)
 
-if y1 == x2:
-    for i in range(x1):
-        for j in range(y2):
-            for k in range(y1):
-                arr3[i][j] += (arr[i][k] * arr2[k][j])
-
-    print("After matrix multiplication :\n", arr3)
-else:
-    print("Array multiplication is not possible.")
+a = np.array([[1, 2], [3, 4]])
+b = np.array([[5, 6], [7, 8]])
+arr = a@b
+print("After matrix multiplication :\n", arr)
